76d93b6574785e66dc41b2b5d76f18dc6316ebc0
abc <abc@example.com>
2024-02-06 00:28:12 +0530

    Remove dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import app from "./app";
-import dotEnv from "dotEnv";
 
-dotEnv.config();
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
     console.log('[server]: Our server is running at http://localhost:' + port);
 });
